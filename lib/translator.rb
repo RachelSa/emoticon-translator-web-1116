8faa9be34d92ed4f895a94aa4b545c65a1721210
Rachel Salois <rachel.salois@gmail.com>
2016-11-14 12:59:57 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,22 @@
 # require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(path)
+  emotis = YAML.load_file(path)
+  get_emoticon = {}
+  get_meaning = {}
+  emotis.each do |k,v|
+    get_emoticon[k] = v[0]
+  end
+
+  emotis.each do |k,v|
+    get_meaning[k] = v[1]
+  end
+
+  emotis.merge!(get_emoticon)
+  emotis.merge!(get_meaning)
+
+  emotis
 end
 
 def get_japanese_emoticon
